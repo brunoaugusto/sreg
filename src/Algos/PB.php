fad2fd5bee9e9bcf3bf760e694f74aaf2c8d9c87
mangierre <mangierre@gmail.com>
2014-12-09 17:21:14 -0200

    New structure of project

---
+++ src/Algos/PB.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Algos;
+
+/**
+ * Brazilian "State Registration" Algorithm for State "PB" (Paraíba)
+ *
+ * @author Bruno Augusto <magnusthorek@gmail.com>
+ * @copyright Copyright (c) 2010-2014 Next Studios
+ * @license http://creativecommons.org/licenses/by/3.0/   Attribution 3.0 Unported
+ *
+ * @see http://www.sintegra.gov.br/Cad_Estados/cad_PB.html
+ */
+class PB {
+
+    /**
+     * Validates given "State Registration" against "Paraíba" Algorithm
+     *
+     * @param string|integer $number
+     *  Given "State Registration" to be validated
+     *
+     * @return bool
+     *  TRUE if given "State Registration" is valid according to "Paraíba" Algorithm
+     *  and FALSE otherwise
+     */
+    public static function validate( $number ) {
+
+        $number = substr( $number, 0, 9 );
+
+        // Checking Length
+
+        if( strlen( $number ) != 9 ) return FALSE;
+
+        // Finding the Check Digit
+
+        $sum = 0;
+
+        for( $w = '98765432', $c = 0; $c < 8; $c += 1 )
+            $sum += $w{ $c } * $number{ $c };
+
+        $cd = ( $sum % 11 );
+
+        $cd = ( ( 11 - $cd ) == 10 || ( 11 - $cd ) == 11 ? 0 : ( 11 - $cd ) );
+
+        // Checking if last character match the Check Digit
+
+        return ( $number{ 8 } == $cd );
+    }
+}
